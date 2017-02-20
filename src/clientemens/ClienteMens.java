e5b26dfccd22758ef49558d08a97f43d87408648
mmoureperez <mariomoureperez@gmail.com>
2017-02-20 10:44:45 +0100

    final sin encriptar

--- src/clientemens/ClienteMens.java
+++ src/clientemens/ClienteMens.java
@@ -25,11 +25,13 @@ public class ClienteMens {
             Socket clienteSocket = new Socket("localhost", 5555);
             System.out.println("Estableciendo la conexion");
 
+            //Creamos flujo de salida cara el server
             PrintStream salida = new PrintStream(clienteSocket.getOutputStream());
+            //Creamos flujo de entrada desde el server
             DataInputStream entrada = new DataInputStream(clienteSocket.getInputStream());
 
-            String mensajeServ;
             
+            //enviamos mensaje de cliente a servidor
             String mensaje = JOptionPane.showInputDialog("Introduzca el mensaje del cliente");
             salida.println(mensaje);
             System.out.println("Mensaje del cliente: "+mensaje);
@@ -37,23 +39,25 @@ public class ClienteMens {
             System.out.println("Mensaje enviado");
             
             String entradaServ;
-            
+            //Bucle que leera lo enviado desde servidor mientras tenga algo qeu leer
             while((entradaServ=entrada.readLine())!=null){
+                
                 System.out.println("Mensaje del servidor : "+entradaServ);
-        
                 mensaje = JOptionPane.showInputDialog("Introduzca el mensaje del Cliente");
                 salida.println(mensaje);
                 System.out.println("Mensaje del cliente: "+mensaje);
                 salida.flush();
+                
+                //cuando utilizamos la palabra clave adios el cliente se cerrará
                     if(mensaje.equals("adios")){
                         break;
                     }
             }
  
-            //leemos el resultado enviado por el servidor
+          
             
-           //System.out.println("resultado de la operacion: " + entrada.readLine());
-           salida.close();
+            //Cerramos flujos de entra y salida
+            salida.close();
             entrada.close();
             System.out.println("Cerrando el socket cliente");
             //cerramos conexión
